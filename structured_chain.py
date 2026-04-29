1c330ccbb450f1845a81530377512c533202e6c1
shahd <shahdtamer247@gmail.com>
2026-04-29 19:03:59 +0300

    update requirements.txt

---
+++ structured_chain.py
@@ -0,0 +1,7 @@
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain,SequentialChain
+import os
+from dotenv import load_dotenv
+
+
